ab601f64767532199a86496ebfcdcae42b95ff88
Alex C-G <alexcg@outlook.com>
2022-05-19 10:22:11 +0200

    refactor: use poetry to package

--- jfc/jfc.py
+++ jfc/jfc.py
@@ -1,10 +1,41 @@
-from argparse import ArgumentError, FileType
 from jina import Client
 from docarray import DocumentArray, Document
 import os
-from helper import preproc, load_config, Formats, default_config
+from jfc.helper import preproc, load_config, Formats, default_config
 import click
 import sys
+from docarray import Document
+import numpy as np
+import yaml
+
+
+# def load_config(filename):
+    # with open(filename, "r") as file:
+        # config = yaml.safe_load(file)
+
+    # return config
+
+
+# def preproc(doc: Document, dims=(64, 64)):
+    # doc.load_uri_to_image_tensor()
+    # doc.tensor = doc.tensor.astype(np.uint8)
+
+    # doc.set_image_tensor_normalization().set_image_tensor_shape(dims)
+
+
+# class Formats:
+    # table = ["csv"]
+    # image = ["jpg", "jpg", "png"]
+
+
+# default_config = {
+    # "host": "http://0.0.0.0:45678",
+    # "indexing": {
+        # "data": "./data",
+        # "num_docs": 1_000_000,
+    # },
+    # "searching": {"image_size": [64, 64]},
+# }
 
 
 def index(host, num_docs, data, **kwargs):
